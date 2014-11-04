64e937edc72d7b8b6600c1011630ffdcfc42d9ad
Jan Vorisek <uicoded@gmail.com>
2014-11-04 00:01:32 -0500

    new accessor properties, edited own vs prototype properties

--- Concepts/getters-and-setters.js
+++ Concepts/getters-and-setters.js
@@ -1,3 +1,57 @@
 // Getters and Setters
 // https://javascriptweblog.wordpress.com/2011/02/28/javascript-object-keys-finally/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects#Defining_getters_and_setters
+
+/* Accessor properties don't contain a value but instead define a function to call when the property
+is read or written to*/
+
+var p1 = {
+	_name: "Karel",
+	_counter: 0,
+
+	get name(){
+		console.log('Name accessed'+ ++this._counter);
+		return this._name;
+	},
+	set name(val){
+		console.log('Setting the internal name to %s', val);
+		this._name = val;
+	}
+};
+
+p1.name; 		// Karel
+p1.name = 'Pepa';	// Setting the internal name to Pepa Pepa
+
+
+/* If you define getter only, then the property becomes read only */
+
+// 'use strict';  // if you use strict it will throw a type error!
+var p1 = {
+	_name: "Karel",
+	_counter: 0,
+
+	get name(){
+		console.log('Name accessed'+ ++this._counter);
+		return this._name;
+	}
+};
+
+p1.name; 			// Karel
+p1.name = 'Pepa';	// Karel and type error in strict mode!
+
+
+/* If you define setter only, then the property becomes write only */
+
+// 'use strict';  // strict or no strict will not have effect on failed read
+var p1 = {
+	_name: "Karel",
+	_counter: 0,
+
+	set name(val){
+		console.log('Setting the internal name to %s', val);
+		this._name = val;
+	}
+};
+
+p1.name; 			// undefined - fails silently regadless of mode
+p1.name = 'Pepa';	// Setting the internal name to Pepa Pepa
--- Functions/hasOwnProperty.js
+++ Functions/hasOwnProperty.js
@@ -1,7 +1,9 @@
 /*
 Object.hasOwnProperty()
 
-Every object descended from Object inherits the hasOwnProperty method. This method can be used to determine whether an object has the specified property as a direct property of that object; unlike the in operator, this method does not check down the object's prototype chain.
+Every object descended from Object inherits the hasOwnProperty method. This method can be used to determine
+whether an object has the specified property as a direct property of that object; unlike the in operator,
+this method does not check down the object's prototype chain.
 
   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
   http://www.bennadel.com/blog/1919-Javascript-s-hasOwnProperty-Method-Is-More-Consistent-Than-The-IN-Operator.htm
@@ -9,14 +11,19 @@ Every object descended from Object inherits the hasOwnProperty method. This meth
 
 */
 
-o = new Object();
-o.prop = 'exists';
+var o1 = {};
+o1.prop = 'exists';
 
-function change() {
-  o.newprop = o.prop;
-  delete o.prop;
-}
+o1.hasOwnProperty('prop');   // returns true
+delete o1.prop;
+o1.hasOwnProperty('prop');   // returns false
 
-o.hasOwnProperty('prop');   // returns true
-change();
-o.hasOwnProperty('prop');   // returns false
+/* compare with in operator which checks for both own properties and prototype properties
+*/
+o1.prop = 'exists again';
+
+('prop' in o1)				// true
+o1.hasOwnProperty('name')	// true
+
+('toString' in o1)				// true
+o1.hasOwnProperty('toString')	// false
